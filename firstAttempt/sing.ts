7a1641d405812848e4b5aa218023d1e75cf242e0
isabelcooper <isabel@triptease.com>
2021-05-21 21:14:32 +0100

    refactor: early exit for 0 bottles

--- firstAttempt/sing.ts
+++ firstAttempt/sing.ts
@@ -1,26 +1,27 @@
 export class Bottles {
-  public sing() : string[] {
+  public sing(): string[] {
     return Bottles.generateNumberArray(99, 0)
       .map(i => Bottles.verse(i)).flat()
   }
 
-  private static verse(numberOfBottles: number) : string[]{
+  private static verse(numberOfBottles: number): string[] {
     const lines: string[] = []
-    if (numberOfBottles > 0) {
-      const plurality = Bottles.getPlural(numberOfBottles)
-      lines.push(`${numberOfBottles} bottle${plurality} of beer on the wall, ${numberOfBottles} bottle${plurality} of beer.`)
-
-      const numberOfBottlesLeft = numberOfBottles - 1
-      if (numberOfBottlesLeft > 0) {
-        const pluralityLeft = Bottles.getPlural(numberOfBottlesLeft)
-        lines.push(`Take one down and pass it around, ${numberOfBottlesLeft || 'no more'} bottle${pluralityLeft} of beer on the wall.`)
-      }
-    } else {
-      lines.push(`Take one down and pass it around, no more bottles of beer on the wall.`)
-      lines.push(`No more bottles of beer on the wall, no more bottles of beer.`)
-      lines.push('Go to the store and buy some more, 99 bottles of beer on the wall.')
+    if (numberOfBottles === 0) {
+      return [
+        `Take one down and pass it around, no more bottles of beer on the wall.`,
+        `No more bottles of beer on the wall, no more bottles of beer.`,
+        'Go to the store and buy some more, 99 bottles of beer on the wall.',
+      ]
     }
 
+    const plurality = Bottles.getPlural(numberOfBottles)
+    lines.push(`${numberOfBottles} bottle${plurality} of beer on the wall, ${numberOfBottles} bottle${plurality} of beer.`)
+
+    const numberOfBottlesLeft = numberOfBottles - 1
+    if (numberOfBottlesLeft > 0) {
+      const pluralityLeft = Bottles.getPlural(numberOfBottlesLeft)
+      lines.push(`Take one down and pass it around, ${numberOfBottlesLeft || 'no more'} bottle${pluralityLeft} of beer on the wall.`)
+    }
     return lines
   };
 
