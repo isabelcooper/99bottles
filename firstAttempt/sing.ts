9c3184a51b053d7aff8a68e9e36f3908c95cc30b
isabelcooper <isabel@triptease.com>
2021-05-21 20:05:55 +0100

    refactor: keep the messy last lines at the end

--- firstAttempt/sing.ts
+++ firstAttempt/sing.ts
@@ -1,21 +1,21 @@
 export const sing = function () {
   const arr = []
 
-  for (let i = 99; i >= 0; i--) {
+  for (let i = 99; i >= 1; i--) {
     const plurality = i > 1 || i === 0 ? 's' : ''
     const numberOfBottles = i > 0 ? i : 'no more'
     arr.push(`${i > 0 ? i : 'No more'} bottle${plurality} of beer on the wall, ${numberOfBottles} bottle${plurality} of beer.`)
 
-    const numberOfBottlesLeft = i > 0 ? i - 1 : 'no more'
-    const pluralityLeft = numberOfBottlesLeft > 1 || numberOfBottlesLeft === 0 ? 's' : ''
-
-    if (numberOfBottlesLeft === 'no more') {
-      arr.push('Go to the store and buy some more, 99 bottles of beer on the wall.')
-    } else {
+    const numberOfBottlesLeft = i - 1
+    if (numberOfBottlesLeft > 0) {
+      const pluralityLeft = numberOfBottlesLeft > 1 ? 's' : ''
       arr.push(`Take one down and pass it around, ${numberOfBottlesLeft || 'no more'} bottle${pluralityLeft} of beer on the wall.`)
     }
-
   }
+
+  arr.push(`Take one down and pass it around, no more bottles of beer on the wall.`)
+  arr.push(`No more bottles of beer on the wall, no more bottles of beer.`)
+  arr.push('Go to the store and buy some more, 99 bottles of beer on the wall.')
 
   return arr
 };
