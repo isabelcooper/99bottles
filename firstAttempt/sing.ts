daa4ea366206ac7a941cf6e54b81257f1a3c2d3e
isabelcooper <isabel@triptease.com>
2021-05-21 20:14:05 +0100

    refactor: try a while and extract plurality

--- firstAttempt/sing.ts
+++ firstAttempt/sing.ts
@@ -1,21 +1,25 @@
 export const sing = function () {
   const arr = []
+  let numberOfBottles = 99
 
-  for (let i = 99; i >= 1; i--) {
-    const plurality = i > 1 || i === 0 ? 's' : ''
-    const numberOfBottles = i > 0 ? i : 'no more'
-    arr.push(`${i > 0 ? i : 'No more'} bottle${plurality} of beer on the wall, ${numberOfBottles} bottle${plurality} of beer.`)
+  while (numberOfBottles > 0) {
+    const plurality = getPlural(numberOfBottles)
+    arr.push(`${numberOfBottles} bottle${plurality} of beer on the wall, ${numberOfBottles} bottle${plurality} of beer.`)
 
-    const numberOfBottlesLeft = i - 1
+    const numberOfBottlesLeft = numberOfBottles - 1
     if (numberOfBottlesLeft > 0) {
-      const pluralityLeft = numberOfBottlesLeft > 1 ? 's' : ''
+      const pluralityLeft = getPlural(numberOfBottlesLeft)
       arr.push(`Take one down and pass it around, ${numberOfBottlesLeft || 'no more'} bottle${pluralityLeft} of beer on the wall.`)
     }
+    numberOfBottles--
   }
-
   arr.push(`Take one down and pass it around, no more bottles of beer on the wall.`)
   arr.push(`No more bottles of beer on the wall, no more bottles of beer.`)
   arr.push('Go to the store and buy some more, 99 bottles of beer on the wall.')
 
   return arr
 };
+
+function getPlural(numberOfBottles: number): string {
+  return numberOfBottles > 1 ? 's' : '';
+}
