a9909f64255b3e79ba98efa57665fc3519e03e31
isabelcooper <isabel@triptease.com>
2021-05-22 16:31:12 +0100

    feat: method to return the first verse

---
+++ walkthrough/Bottles.test.ts
@@ -0,0 +1,12 @@
+import {BottlesSM} from "./Bottles";
+
+describe('Bottles', () => {
+  test('the first verse', () => {
+    const expected = '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n';
+    expect(new BottlesSM().verse(99)).toBe(expected);
+  });
+});
+
---
+++ walkthrough/Bottles.ts
@@ -0,0 +1,6 @@
+export class BottlesSM {
+  verse(_number: number) {
+    return '99 bottles of beer on the wall, ' + '99 bottles of beer.\n' +
+    'Take one down and pass it around, ' + '98 bottles of beer on the wall.\n'
+  }
+}
